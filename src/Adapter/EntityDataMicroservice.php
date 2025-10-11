190db56fc9f7ede3f7d7ddcdc3ac5faac63a8fa7
Daniel Dahme <info@base3.de>
2025-10-11 09:43:20 +0200

    microservice adapter added

---
+++ src/Adapter/EntityDataMicroservice.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace ResourceFoundation\Adapter;
+
+use Base3\Microservice\AbstractMicroservice;
+use ResourceFoundation\Api\IEntityDataService;
+
+class EntityDataMicroservice extends AbstractMicroservice implements IEntityDataService {
+
+	public function __construct(private readonly IEntityDataService $entityDataService) {}
+
+	// Implementation of IEntityDataService
+
+	public function getEntries(array $options = []): array {
+		return $this->entityDataService->getEntries($options);
+	}
+
+	public function getEntry(int|string $id, array $options = []): ?array {
+		return $this->entityDataService->getEntry($id, options);
+	}
+
+	public function saveEntry(array $data): int|string {
+		return $this->entityDataService->saveEntry($data);
+	}
+
+	public function deleteEntry(int|string $id): bool {
+		return $this->entityDataService->deleteEntry($id);
+	}
+}
